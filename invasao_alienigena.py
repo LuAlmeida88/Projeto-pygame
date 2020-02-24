1f806ad3b3d017aeeab5ffdbae83b8398059ee9c
LuAlmeida88 <56369238+LuAlmeida88@users.noreply.github.com>
2020-02-24 13:56:31 -0300

    Add files via upload
    Criado a cor da tela

--- invasao_alienigena.py
+++ invasao_alienigena.py
@@ -1,19 +1,20 @@
 import sys
 import pygame
+from pygame.locals import*
 
 def iniciar_jogo():
-    # Inicializa  o jogo e cria um objeto para a tela
     pygame.init()
-screen = pygame.display.set_mode((1200, 800))
-pygame.display.set_caption("Invasão Alienigena")
-
-# Inicia o laço principal do jogo
-while True:
-    #Observa os elementos de teclado e mouse
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-                #Deixa a tela mais recente visivel 
-                pygame.display.flip()
+#Define o tamanho da tela
+tela = pygame.display.set_mode((1200, 800))
+Clock = pygame.time.Clock()
+continuar = True
+#Inicia o laço do jogo
+while(continuar):
+    Clock.tick(60)
+    for event in pygame.event.get():
+        if event.type ==QUIT:
+            continuar = False
+#Define a cor de fundo da tela pygame
+    tela.fill((230, 230, 230))
+    pygame.display.flip()
 iniciar_jogo()
